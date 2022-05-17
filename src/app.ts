9988693e60ad8fec76171c2c37ebea7cdb6ce695
andre <andrepassoni98@gmail.com>
2022-05-17 13:57:33 -0300

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import "express-async-errors";
+import cors from "cors";
 import errorHandler from "./errors/handleError.middleware";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(errorHandler);
 
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-import app from "./app"
-import AppDataSource from "./data-source"
-;(async () => {
+import app from "./app";
+import AppDataSource from "./data-source";
+
+(async () => {
   await AppDataSource.initialize()
     .then(() => {
-      console.log("\nData source initiliazed")
+      console.log("\nData source initiliazed");
     })
     .catch((err) => {
-      console.error("Error during Data Source initialization", err)
-    })
+      console.error("Error during Data Source initialization", err);
+    });
 
-  const PORT = process.env.PORT || 3000
+  const PORT = process.env.PORT || 3000;
 
-  app.listen(PORT, () => console.log(`\nRunning at http://localhost:${PORT}\n`))
-})()
+  app.listen(PORT, () =>
+    console.log(`\nRunning at http://localhost:${PORT}\n`)
+  );
+})();
