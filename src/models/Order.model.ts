5c5da2b6214382beca7aa833d4ddac74cd8ff8b5
Igor Cenzi <igor.cenzi2@gmail.com>
2022-05-18 15:00:12 -0300

    fix: timestamps columns added on order-model

--- src/models/Order.model.ts
+++ src/models/Order.model.ts
@@ -1,22 +1,39 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
-export enum OrderStatus{
-    PENDING = 'pending',
-    READY = 'ready',
-    SERVED = 'served'
+export enum OrderStatus {
+  PENDING = "pending",
+  READY = "ready",
+  SERVED = "served",
 }
 
-@Entity('Order')
-export default class Order{
-    @PrimaryGeneratedColumn('uuid')
-    id: string
+@Entity("Order")
+export default class Order {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column({name: 'table', type: 'varchar', length: '3'})
-    table: string
+  @Column({ name: "table", type: "varchar", length: "3" })
+  table: string;
 
-    @Column({name: 'status', type: 'enum', enum: OrderStatus, default: OrderStatus.PENDING})
-    status: OrderStatus
+  @Column({
+    name: "status",
+    type: "enum",
+    enum: OrderStatus,
+    default: OrderStatus.PENDING,
+  })
+  status: OrderStatus;
 
-    @Column({name: 'total', type: 'decimal', precision: 8, scale: 2})
-    total: Number
+  @Column({ name: "total", type: "decimal", precision: 8, scale: 2 })
+  total: Number;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
 }
