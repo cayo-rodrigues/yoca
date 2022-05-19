bc097f7cbac3081ad340584295d0abe65efef5bb
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-05-19 17:03:57 -0300

    refact: use the parameter "value" instead of "originalValue" in createIngredientSchema yup.transform

--- src/schemas/ingredients/createIngredient.schema.ts
+++ src/schemas/ingredients/createIngredient.schema.ts
@@ -10,26 +10,22 @@ const createIngredientSchema = {
           .string()
           .max(164, "name field has a max of 164 characters")
           .required("name field is required")
-          .transform((value, originalValue) =>
-            originalValue.toLowerCase().trim()
-          ),
+          .transform((value: string) => value.toLowerCase().trim()),
         measure: yup
           .string()
           .max(3, "measure field has a max of 3 characters")
           .required("measure field is required")
-          .transform((value, originalValue) =>
-            originalValue.toLowerCase().trim()
-          ),
+          .transform((value: string) => value.toLowerCase().trim()),
         amount: yup
           .number()
           .positive("amount field can't be negative")
           .required("amount field is required")
-          .transform((value, originalValue) => roundToTwo(originalValue)),
+          .transform((value: number) => roundToTwo(value)),
         amountMax: yup
           .number()
           .positive("amountMax field can't be negative")
           .required("amountMax field is required")
-          .transform((value, originalValue) => roundToTwo(originalValue)),
+          .transform((value: number) => roundToTwo(value)),
         amountMin: yup
           .number()
           .positive("amountMin field can't be negative")
@@ -38,9 +34,7 @@ const createIngredientSchema = {
             yup.ref("amountMax"),
             "amountMin should be less than amountMax"
           )
-          .transform((value, originalValue: number) =>
-            roundToTwo(originalValue)
-          ),
+          .transform((value: number) => roundToTwo(value)),
       }),
       validateOptions: {
         abortEarly: false,
