31b6d0632df969a7369af469d6580af95bc125d5
Victor Ramon <victorrgds02@gmail.com>
2022-05-21 10:22:14 -0300

    test: integration tests for both feedbacks types crud routes

---
+++ src/__tests__/integration/feedbacks/product/listOneProductFeedback.spec.ts
@@ -0,0 +1,61 @@
+import { DataSource } from "typeorm";
+import AppDataSource from "../../../../data-source";
+import app from "../../../../app";
+import request from "supertest";
+
+import * as uuid from "uuid";
+jest.mock("uuid");
+
+describe("GET - /feedbacks/product/:id", () => {
+  let connection: DataSource;
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => (connection = res))
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+      });
+  });
+
+  const mockProductFeedback = {
+    product: "Pizza",
+    description:
+      "Massa muito grossa, não consegui bater meu recorde de 45 fatias :(",
+    rating: 2,
+  };
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it("Should be able to list one product feedback", async () => {
+    const uuidSpy = jest.spyOn(uuid, "v4");
+    uuidSpy.mockReturnValueOnce("prod-fb-uuid");
+
+    const prodFeedbackResponse = await request(app)
+      .post("/feedbacks/product")
+      .send(mockProductFeedback);
+
+    const listOneGenFeedback = await request(app).get(
+      "/feedbacks/product/prod-fb-uuid"
+    );
+
+    expect(listOneGenFeedback.status).toBe(200);
+    expect(listOneGenFeedback.body).toMatchObject({
+      id: "prod-fb-uuid",
+      ...mockProductFeedback,
+    });
+  });
+  it("Should not be able to list one product feedback sending unexistent id", async () => {
+    const listOneGenFeedback = await request(app).get(
+      "/feedbacks/product/unex-prod-fb-uuid"
+    );
+
+    expect(listOneGenFeedback.status).toBe(404);
+    expect(listOneGenFeedback.body).toEqual(
+      expect.objectContaining({
+        message: "Feedback not found",
+      })
+    );
+  });
+});
