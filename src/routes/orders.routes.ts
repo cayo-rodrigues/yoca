90b49a9d6b8aa021df6edba59f6757bb5ca45d4b
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2022-05-24 12:48:28 -0300

    feat: add access level verification in orders routes

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
 import { expressYupMiddleware } from "express-yup-middleware";
 import OrdersController from "../controllers/Orders.controller";
+import verifyAccessLevelMiddleware from "../middlewares/verifyAccessLevel.middleware";
 import createOrderSchema from "../schemas/orders/createOrder.schema";
 
 const ordersRoutes = Router();
 
 ordersRoutes.post(
   "",
+  verifyAccessLevelMiddleware(4),
   expressYupMiddleware({ schemaValidator: createOrderSchema }),
   OrdersController.store
 );
-ordersRoutes.get("", OrdersController.index);
+ordersRoutes.get("", verifyAccessLevelMiddleware(4), OrdersController.index);
 ordersRoutes.patch("/:id", OrdersController.update);
-ordersRoutes.delete("/:id", OrdersController.delete);
+ordersRoutes.delete(
+  "/:id",
+  verifyAccessLevelMiddleware(4),
+  OrdersController.delete
+);
 
 export default ordersRoutes;
