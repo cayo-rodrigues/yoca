26d65e24ccd1ad8d22592b0bf5471018ad564a37
leonardoliska <leo.liska@hotmail.com>
2022-05-25 09:36:56 -0300

    refactor: change orders route middlewares and schemas

--- src/schemas/orders/updateOrder.schema.ts
+++ src/schemas/orders/updateOrder.schema.ts
@@ -1,22 +1,13 @@
 import * as yup from "yup";
 
-const updateOrderSchema = {
-  schema: {
-    body: {
-      yupSchema: yup.object().shape({
-        status: yup
-          .string()
-          .oneOf(
-            ["pending", "ready", "served"],
-            'Field status must be "pending", "ready" or "served"'
-          )
-          .required("Field status is required"),
-      }),
-      validateOptions: {
-        abortEarly: false,
-      },
-    },
-  },
-};
+const updateOrderSchema = yup.object().shape({
+  status: yup
+    .string()
+    .oneOf(
+      ["pending", "ready", "served"],
+      'Field status must be "pending", "ready" or "served"'
+    )
+    .required("Field status is required"),
+});
 
 export default updateOrderSchema;
