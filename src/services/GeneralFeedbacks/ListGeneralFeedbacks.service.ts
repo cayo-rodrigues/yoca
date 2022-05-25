51058a6cf7ae46489d45cb72ea98a803b964a93e
felipe <felipesindeaux@outlook.com>
2022-05-25 15:36:13 -0300

    feat: generalFeedbacks get order pagination

--- src/services/GeneralFeedbacks/ListGeneralFeedbacks.service.ts
+++ src/services/GeneralFeedbacks/ListGeneralFeedbacks.service.ts
@@ -1,14 +1,24 @@
 import AppDataSource from "../../data-source";
+import { IList } from "../../interfaces/List.interface";
 import GeneralFeedback from "../../models/GeneralFeedback.model";
 
 class ListGeneralFeedbacksService {
-  static async execute() {
+  static async execute({ per_page, page }: IList) {
     const generalFeedbackRepository =
       AppDataSource.getRepository(GeneralFeedback);
 
-    const feedbacks = await generalFeedbackRepository.find();
+    if (!per_page) {
+      per_page = 20;
+    }
 
-    return feedbacks;
+    if (!page) {
+      page = 1;
+    }
+
+    return await generalFeedbackRepository.find({
+      skip: per_page * (page - 1),
+      take: per_page,
+    });
   }
 }
 
