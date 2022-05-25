61953239f68880b3f6b10b46e2f92de53fffc055
leonardoliska <leo.liska@hotmail.com>
2022-05-25 09:28:18 -0300

    refactor: change bill middlewares

--- src/routes/bills.routes.ts
+++ src/routes/bills.routes.ts
@@ -1,23 +1,20 @@
 import { Router } from "express";
-import { expressYupMiddleware } from "express-yup-middleware";
+
 import BillsController from "../controllers/Bills.controller";
+
+import validateUUIDMiddleware from "../middlewares/validateUUID.middleware";
 import verifyAccessLevelMiddleware from "../middlewares/verifyAccessLevel.middleware";
-import validateUUIDSchema from "../schemas/validateUUID.schema";
 
 const billsRoutes = Router();
 
 billsRoutes.post("/", verifyAccessLevelMiddleware(3), BillsController.store);
 billsRoutes.get("/", verifyAccessLevelMiddleware(3), BillsController.index);
 
-billsRoutes.get(
-  "/:id",
-  expressYupMiddleware({ schemaValidator: validateUUIDSchema }),
-  verifyAccessLevelMiddleware(3),
-  BillsController.show
-);
+billsRoutes.use("/:id", validateUUIDMiddleware);
+
+billsRoutes.get("/:id", verifyAccessLevelMiddleware(3), BillsController.show);
 billsRoutes.patch(
   "/:id",
-  expressYupMiddleware({ schemaValidator: validateUUIDSchema }),
   verifyAccessLevelMiddleware(3),
   BillsController.update
 );
